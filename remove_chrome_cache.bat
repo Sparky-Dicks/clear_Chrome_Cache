7925530c905288adbb7b62db3c770981c0385bac
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-01-23 10:57:44 +0200

    Updating to wipe Documents as well
    I am just adding some lines to make sure the documents that people make and folders also get cleared. This prevents a new user from getting the "complete work"

--- remove_chrome_cache.bat
+++ remove_chrome_cache.bat
@@ -1,13 +1,34 @@
-@echo off
 
-title Reset Google
+title WipePcData_
 
-echo Lets delete google cache...
+cd /d %USERPROFILE%\Downloads
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 
-pause
+cd /d %USERPROFILE%\Pictures
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+cd /d %USERPROFILE%\Documents
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+cd /d %USERPROFILE%\3D Objects
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+cd /d %USERPROFILE%\Desktop
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+cd /d %USERPROFILE%\Music
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+cd /d %USERPROFILE%\Videos
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
+
 set name="User Data"
-rmdir /s %USERPROFILE%\AppData\Local\Google\Chrome\%name%
+rmdir /s/q %USERPROFILE%\AppData\Local\Google\Chrome\%name%
 cd %USERPROFILE%\AppData\Local\Google\Chrome
 mkdir %name%
+cd /d %USERPROFILE%
+
+cls
 echo Operation complete
 pause
