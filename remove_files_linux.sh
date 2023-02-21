71c13c9302e4f352bf4b54b9dee367210a1e73ac
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-21 08:49:46 +0200

    Finalizing

--- remove_files_linux.sh
+++ remove_files_linux.sh
@@ -11,18 +11,10 @@ for dir in /home/*; do
   # Check if the directory is an exception
   if [[ "${exceptions[@]}" =~ $(basename "$dir") ]]; then
     # Remove all files and subdirectories in the exception directory
-    sudo find "$dir" -mindepth 1 -maxdepth 1 ! -name ".*" -exec rm -rf {} +
+    sudo rm -rf "$dir"/*
   else
     # Remove all files and subdirectories in non-exception directories except hidden files
     sudo find "$dir" -mindepth 1 -maxdepth 1 ! -name ".*" -exec rm -rf {} +
   fi
 done
-
-# Check if the script is in /bin
-if [ "$(pwd)" != "/bin" ]; then
-  # Move the script to /bin and make it executable
-  sudo mv "$0" /bin
-  sudo chmod +x /bin/$(basename "$0")
-  # Run the script from /bin
-  /bin/$(basename "$0")
-fi
+mkdir /home/wethinkcode/Downloads /home/wethinkcode/Desktop /home/wethinkcode/Documents /home/wethinkcode/Music /home/wethinkcode/Pictures /home/wethinkcode/Public /home/wethinkcode/Templates /home/wethinkcode/Videos
