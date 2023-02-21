8de578edec1e8b97197867fa3e2bb0230528e215
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-21 08:55:44 +0200

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -9,6 +9,7 @@ else
   # Move the script to /bin and make it executable
   sudo mv "$script_path" /bin
   sudo chmod +x /bin/remove_files_linux.sh
-  echo "Script moved to /bin and made executable."
+  echo "Script moved and made executable."
   remove_files_linux.sh
+  echo "Thank you, thats all you need to do."
 fi
