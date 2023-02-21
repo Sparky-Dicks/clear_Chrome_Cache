ab546b66fd6ea26e416c3fd903f55a18c078bb5b
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-21 16:26:02 +0200

    Testing the expect method

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,7 +1,29 @@
 sudo apt update -y
+expect "assword"
+send -- "wtc"
 sudo apt install openssh-server
-sudo ufw allow ssh
-adduser adminsparky
+sudo ufw allow ssh100
+sudo adduser adminsparky
+
+expect "assword"
+send -- "my_password"
+expect "again"
+send -- "my_password"
+
+expect "some name1"
+send -- "\n"
+expect "some name2"
+send -- "\n"
+expect "some name3"
+send -- "\n"
+expect "some name4"
+send -- "\n"
+expect "some name5"
+send -- "\n"
+
+expect "details"
+send -- "y\r"
+
 usermod -aG sudo adminsparky
 clear
 ifconfig
