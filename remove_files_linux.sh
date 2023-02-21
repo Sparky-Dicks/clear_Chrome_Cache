ad3fe88aa6444807b3bb7b2b1801b7e3bf11a63a
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-21 08:25:45 +0200

    Create remove_files_linux.sh

---
+++ remove_files_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Remove everything in the specified folders
+sudo rm -rf /home/wethinkcode/.mozilla/firefox/*
+
+# Create a list of exceptions
+exceptions=(Downloads Desktop Documents Music Pictures Public Templates Videos)
+
+# Loop through the directories in /home
+for dir in /home/*; do
+  # Check if the directory is an exception
+  if [[ "${exceptions[@]}" =~ $(basename "$dir") ]]; then
+    # Remove all files and subdirectories in the exception directory
+    sudo rm -rf "$dir"/*
+  else
+    # Remove all files and subdirectories in non-exception directories except hidden files
+    sudo find "$dir" -mindepth 1 -maxdepth 1 ! -name ".*" -exec rm -rf {} +
+  fi
+done
