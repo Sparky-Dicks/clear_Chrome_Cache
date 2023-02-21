465273b5f90c3431d5da5342b1516b1001e29ec9
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-21 12:04:32 +0200

    Update install_ssh.sh

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt install openssh-server
 sudo ufw allow ssh
-sudo systemctl status ssh
 adduser adminsparky
 usermod -aG sudo adminsparky
 clear
-su – username
+ifconfig
+whoami
