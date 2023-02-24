623081dfff65893f2c69891207c5f326a38ce1fd
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-24 21:45:42 +0200

    Update to clear browser data

--- remove_files_linux.sh
+++ remove_files_linux.sh
@@ -3,6 +3,34 @@
 # Remove everything in the specified folders
 sudo rm -rf /home/wethinkcode/.mozilla/firefox/*
 
+# List of browser names to clear browsing data for
+browsers=("google-chrome" "chromium" "firefox" "brave-browser")
+
+for browser in "${browsers[@]}"; do
+    # Check if browser is installed
+    if command -v "$browser" >/dev/null 2>&1; then
+        # Clear browsing data for the browser
+        case "$browser" in
+            "google-chrome")
+                rm -rf "${HOME}/.config/google-chrome/Default/*"
+                ;;
+            "chromium")
+                rm -rf "${HOME}/.config/chromium/Default/*"
+                ;;
+            "firefox")
+                rm -rf "${HOME}/.mozilla/firefox/*.default-release/*"
+                ;;
+            "brave-browser")
+                rm -rf "${HOME}/.config/BraveSoftware/Brave-Browser/Default/*"
+                ;;
+        esac
+        echo "Cleared browsing data for $browser"
+    else
+        echo "$browser is not installed"
+    fi
+done
+
+
 # Create a list of exceptions
 exceptions=(Downloads Desktop Documents Music Pictures Public Templates Videos)
 
