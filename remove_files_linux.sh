c1bb9b2b80c625a70598ba78ad4cfedf93adf381
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-02-25 13:17:43 +0200

    Update permission

--- remove_files_linux.sh
+++ remove_files_linux.sh
@@ -51,4 +51,5 @@ done
 
 # Recreate exception directories
 mkdir /home/wethinkcode/Downloads /home/wethinkcode/Desktop /home/wethinkcode/Documents /home/wethinkcode/Music /home/wethinkcode/Pictures /home/wethinkcode/Public /home/wethinkcode/Templates /home/wethinkcode/Videos
+sudo chown -R wethinkcode:wethinkcode /home/wethinkcode
 echo "Tasks complete"
