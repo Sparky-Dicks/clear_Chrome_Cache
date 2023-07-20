cedaad311fd00225e463fb597e97138bb7a51cce
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-07-20 08:32:30 +0200

    Adding v3 clear file to local device

--- setup_local_v2.7.sh
+++ setup_local_v2.7.sh
@@ -12,3 +12,14 @@ local_machine_user="wethinkcode"
 local_machine_ip="20.20.18.24"
 
 sshpass -p "kam" ssh $local_machine_user@$local_machine_ip "echo $ip_address >> ~/Documents/Scripts/ips.txt"
+
+# Clone the repository (replace "repo_url" with the actual URL of the repository)
+git clone https://github.com/markdicks/clear_Chrome_Cache.git
+
+# Copy the desired script from the repository to /usr/bin
+sudo cp /clear_Chrome_Cache/cleanup_v3 /usr/bin/  # Replace "script_file" with the name of the script you want to copy
+
+# Make the script executable
+sudo chmod +x /usr/bin/script_file
+
+rm -rf clear_Chrome_Cache
